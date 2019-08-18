74e54fb31cf76929dca6412d1fab559b9dcebed1
taghrid <taghrid@pentavalue.com>
2019-08-18 17:19:58 +0200

    second

--- fabcar/teardown.sh
+++ fabcar/teardown.sh
@@ -1,4 +1,4 @@
 docker kill $(docker ps -aq)
 docker rm $(docker ps -aq)
-docker rmi $(docker ps -a | awk '($2 ~ /dev-peer.*.fabcar.*/) {print $1}')
+docker rmi $(docker images | awk '($1 ~ /dev-peer.*.fabcar.*/) {print $1}')
 rm javascript/wallet -r
